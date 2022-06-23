3f17a25442782db72ec61a04e80b5305092c6dd8
bvanhaitsma <bvanhaitsma@reviveourhearts.com>
2022-06-22 22:34:23 -0400

    added styled components styling

--- src/components/AccountBalance/AccountBalance.jsx
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './AccountBalance.css';
+import styled from 'styled-components';
+
+const Section = styled.section`
+  background-color: black;
+  font-size: 2rem;
+  text-align: left;
+  padding: 1.5rem 0 1.5rem 5rem;
+`;
 
 export class AccountBalance extends Component {
   render() {
     return (
-      <section>
-        $ {this.props.amount}
-      </section>
+      <Section>
+        Balance: ${this.props.amount}
+      </Section>
     );
   }
 }
