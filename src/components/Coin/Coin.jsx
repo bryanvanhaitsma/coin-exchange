6cfde69987dd3402d35d98081c644f808fb83c76
bvanhaitsma <bvanhaitsma@reviveourhearts.com>
2022-07-04 21:48:56 -0400

    updated coin.jsx to functional component

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -9,29 +9,28 @@ const Td = styled.td`
   font-size: 1.4rem;
 `;
 
-export default class Coin extends Component {
+// export default class Coin extends Component {
+export default function Coin(props) {
 
-  handleClick = (event) => {
+  const handleClick = (event) => {
     // Prevent default action
     event.preventDefault();
-    this.props.handleRefresh(this.props.tickerId);
+    props.handleRefresh(props.tickerId);
   }
   
-  render() {
-    return (
-      <tr>
-        <Td>{this.props.name}</Td>
-        <Td>{this.props.ticker}</Td>
-        <Td>${this.props.price}</Td>
-        {this.props.showBalance ? <Td>{this.props.balance}</Td> : null }
-        <Td>
-          <form action="#" method="POST">
-            <button onClick={this.handleClick}>Refresh</button>
-          </form>
-        </Td>
-      </tr>
-    );
-  }
+  return (
+    <tr>
+      <Td>{props.name}</Td>
+      <Td>{props.ticker}</Td>
+      <Td>${props.price}</Td>
+      {props.showBalance ? <Td>{props.balance}</Td> : null }
+      <Td>
+        <form action="#" method="POST">
+          <button onClick={handleClick}>Refresh</button>
+        </form>
+      </Td>
+    </tr>
+  );
 }
 
 Coin.propTypes = {
