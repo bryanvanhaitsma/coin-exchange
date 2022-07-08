d7554ed1882deb369f9ad6e8f3171c97e6504f04
bvanhaitsma <bvanhaitsma@reviveourhearts.com>
2022-07-07 22:06:07 -0400

    Updates

---
+++ precache-manifest.664f927f8e35f3e4732437a6651a5e3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83a832a9785f001203ac",
+    "url": "/coin-exchange/static/css/main.321e7ad5.chunk.css"
+  },
+  {
+    "revision": "83a832a9785f001203ac",
+    "url": "/coin-exchange/static/js/main.83a832a9.chunk.js"
+  },
+  {
+    "revision": "dca74a035a2640b5084a",
+    "url": "/coin-exchange/static/js/1.dca74a03.chunk.js"
+  },
+  {
+    "revision": "98207c9a858a80f374eb",
+    "url": "/coin-exchange/static/js/2.98207c9a.chunk.js"
+  },
+  {
+    "revision": "94608f115c7582681c34",
+    "url": "/coin-exchange/static/js/runtime~main.94608f11.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/coin-exchange/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "cbac5272b96b21f1d1416a711aa390f1",
+    "url": "/coin-exchange/index.html"
+  }
+];
